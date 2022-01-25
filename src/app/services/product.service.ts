09ec499c2e51d8936c0392a74c099def3c7076f2
POND666 <suphakitpyto3321@gmail.com>
2022-01-25 17:53:21 +0700

    first commit

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private url = `${environment.serviceUrl}/product`
+  constructor(private http: HttpClient) { }
+
+  //Get all Product
+  getProduct(): any{
+    return this.http.get<any>(this.url);
+  }
+
+  //Get by ID
+  getProductById(id: any){
+    let getUrl = `${this.url}/${id}`;
+    return this.http.get<any>(getUrl);
+  }
+
+  //Add Product
+  addProduct(product: any){
+    return this.http.post<any>(this.url,product)
+    .pipe(map((res)=>{
+      return res;
+    }))
+  }
+
+  //Update Product
+  updateProdcut(product: any,id: any){
+    let getUrl = `${this.url}/${id}`;
+    return this.http.put<any>(getUrl,product)
+    .pipe(map((res)=>{
+      return res;
+    }))
+  }
+
+  //Delete Product
+  deleteProduct(id: any){
+    let getUrl = `${this.url}/${id}`;
+    return this.http.delete<any>(getUrl);
+  }
+}
